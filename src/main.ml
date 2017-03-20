b43fd77b68b9c90024cd39e40070af39e8b4d2f8
glennsl <glenn@slotte.net>
2017-03-20 12:43:12 +0100

    tweaked

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,16 @@
-module A = Alias
+type t
+
+let map f = function
+| Some v -> Some (f v)
+| None -> None
+
+external make : ?foo:string -> unit -> t = "" [@@bs.obj]
+
+let a = make ()
+let b = make ~foo:"42" ()
+
+let make ?foo:(foo: int option) = 
+	make ?foo:(map string_of_int foo)
+
+let a' = make ()
+let b' = make ~foo:42 ()
